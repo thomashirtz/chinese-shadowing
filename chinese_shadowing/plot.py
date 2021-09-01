79f9ddb4ba257a1446634aebada3c1845092a7d8
Thomas <hirtzt@gmail.com>
2021-09-01 21:40:38 +0200

    Add type annotations

--- chinese_shadowing/plot.py
+++ chinese_shadowing/plot.py
@@ -3,11 +3,14 @@ from typing import Optional
 import matplotlib.pyplot as plt
 
 
-def plot_amplitude(audio, rate, title: Optional = None):
+def plot_amplitude(
+        audio: np.array,
+        rate: int,
+        title: Optional[str] = None
+) -> None:
     time = np.arange(len(audio)) / rate
     plt.plot(time, audio)
     plt.xlabel('Time [s]')
     plt.ylabel('Amplitude')
-    if title:
-        plt.title(label=title)
+    plt.title(label=title)
     plt.show()
